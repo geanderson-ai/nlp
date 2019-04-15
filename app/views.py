b25e257dfb7aa2753a2e917eb35b5febb3146c7f
Geanderson Lenz <geandersonsl@gmail.com>
2019-04-15 00:56:44 -0300

    first forms

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # create to myself
-from flask import render_template
+from flask import render_template, flash
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder import ModelView, BaseView
 from app import appbuilder, db
@@ -8,10 +8,13 @@ from app import appbuilder, db
 from flask_appbuilder import AppBuilder, expose, BaseView, has_access
 from app import appbuilder
 
+from flask_appbuilder import AppBuilder, BaseView, expose, has_access
+from app import appbuilder
 
 
 class MyView(BaseView):
-    route_base = "/myview"
+
+    default_view = 'method1'
 
     @expose('/method1/')
     @has_access
@@ -20,13 +23,13 @@ class MyView(BaseView):
         # and return to previous page or index
         return 'Hello'
 
-    @expose('/method2/<string:param2>')
+    @expose('/method2/<string:param1>')
     @has_access
-    def method3(self, param2):
+    def method2(self, param1):
         # do something with param1
-        # and render it
-        param2 = 'Goodbye Mr. %s' % (param2)
-        return param2
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        return param1
 
     @expose('/method3/<string:param1>')
     @has_access
@@ -38,14 +41,30 @@ class MyView(BaseView):
         return self.render_template('method3.html',
                                param1 = param1)
 
-
-appbuilder.add_view_no_menu(MyView())
-appbuilder.add_link("Method1", href='/myview/method1/', category='My View')
+appbuilder.add_view(MyView, "Method1", category='My View')
 appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
 appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
 
+from flask_appbuilder import SimpleFormView
+from flask_babel import lazy_gettext as _
+from .forms import MyForm
 
 
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.field1.data = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('My form View'),
+                     category="My Forms", category_icon="fa-cogs")
 
 """
     Create your Views::
